707d7714748d35fb925e1ae164588739615dda74
xlli666 <543011684@qq.com>
2019-03-27 09:50:47 +0800

    子模块Swagger参数添加

--- dc-order/src/main/java/com/cloud/demo/order/config/OrderServiceSwaggerConfig.java
+++ dc-order/src/main/java/com/cloud/demo/order/config/OrderServiceSwaggerConfig.java
@@ -4,25 +4,43 @@ import com.cloud.demo.common.utils.ConstantUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class OrderServiceSwaggerConfig {
     @Bean
     public Docket createRestApi() {
+        ParameterBuilder tokenParam = new ParameterBuilder();
+        List<Parameter> parameters = new ArrayList<>();
+        tokenParam
+                .name(ConstantUtils.SWG_VALID)
+                .description("令牌")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+        parameters.add(tokenParam.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 //.host("localhost:8089")
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.cloud.demo.order.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(parameters);
     }
 
     private ApiInfo apiInfo() {
